f2ddaaf6f719da71c625c71e151bd01e8ef6de19
JParraW <josanpargar@gmail.com>
2023-02-26 20:44:14 +0100

    feat: create calendar component feat: inject calendar component with data from json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,36 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Calendar from './components/calendar.component';
+
 
 function App() {
+    const [dataLoaded, setDataLoaded] = useState(false);
+    const [data, setData] = useState<any[]>([]);
 
     useEffect(() => {
-        getData()
-    }, [])
-
-    const getData = () => {
-        fetch('calendar.json'
-            , {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-            }
-        )
-            .then(function (response) {
-                console.log(response)
-                return response.json();
+        fetch('calendar.json')
+            .then((response) => response.json())
+            .then((calendar) => {
+                setData(calendar[0].events);
+                setDataLoaded(true);
             })
-            .then(function (myJson) {
-                console.log(myJson);
+            .catch((error) => {
+                console.error("Error:", error);
             });
+    }, []);
+
+    if (dataLoaded) {
+        return (
+            <div className="app">
+                {data.map(event => <Calendar value={event} key={event.id}/>)}                
+            </div>
+        );
+    } else {
+        return (
+            <div className="app">
+            </div>
+        );
     }
-
-    const [data, setData] = useState([]);
-
-  return (
-      <div className="app">
-      </div>
-  );
 }
 
 export default App;
